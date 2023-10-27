aba3360c027fdc8925b16a1b10595bd0363fd7b3
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-10-27 15:30:56 -0400

    create database file (tables)

--- server/models.py
+++ server/models.py
@@ -1,10 +1,10 @@
-from flask_sqlachemy import SQLAlchemy
-from sqlalchemy import Metadata
-from sqlalchemy_serializer import SerializeMixin
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.orm import validates
 
-metadata = Metadata ( naming_convention = {
+metadata = MetaData ( naming_convention = {
     "fk" : "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
     "uq" : "uq_%(table_name)s_%(column_0_name)s",
     "uq" : "ck_%(table_name)s_%(constraint_name)s",
@@ -14,12 +14,11 @@ metadata = Metadata ( naming_convention = {
 db = SQLAlchemy(metadata = metadata)
 
 
-id = db.Column (db.Integer, primary_key = True)
 
 #-----  Users  -----#
 class User ( db.Model ) :
     __tablename__ = "users"
-    id
+    id = db.Column (db.Integer, primary_key = True)
     name = db.Column (db.String, nullable = False)
     last_name = db.Column (db.String, nullable = False)
     e_mail = db.Column (db.String, nullable = False)
@@ -29,7 +28,7 @@ class User ( db.Model ) :
 #-----  Contractors  -----#
 class Contractor ( db.Model ) :
     __tablename__ = "contractors"
-    id
+    id = db.Column (db.Integer, primary_key = True)
     company_name = db.Column (db.String, nullable = False)
     address = db.Column (db.String)
     secondary_address = db.Column (db.String)
@@ -37,26 +36,31 @@ class Contractor ( db.Model ) :
     skills = db.Column (db.String, nullable = False)
     age = db.Column (db.Integer, nullable = False)
 
+    user_id = db.Column (db.Integer, db.ForeignKey ("users.id"))
+
 #-----  Reviews  -----#
 class Review ( db.Model ) :
     __tablename__ = "reviews"
-    id
+    id = db.Column (db.Integer, primary_key = True)
     stars = db.Column (db.Integer, nullable = False)
     text = db.Column (db.String, nullable = False)
 
+    user_id = db.Column (db.Integer, db.ForeignKey ("users.id"))
+    contractor_id = db.Column (db.Integer, db.ForeignKey ("contractors.id"))
+
 #-----  Likes  -----#
-class Like ( db.Model ) :
+"""class Like ( db.Model ) :
     __tablename__ = "likes"
-    id
+    id = db.Column (db.Integer, primary_key = True)
     user_id = db.Column ()
-    post_id = db.Column ()
+    post_id = db.Column ()"""
 
 #-----  Comments  -----#
-class Comment ( db.Model ) :
+"""class Comment ( db.Model ) :
     __tablename__ = "comments"
-    id
+    id = db.Column (db.Integer, primary_key = True)
     comment_id = db.Column ()
     post_id = db.Column ()
-    user_id = db.Column ()
-    
+    user_id = db.Column ()"""
+
 #-----    -----#
