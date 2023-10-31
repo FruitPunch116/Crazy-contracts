e57fa99bba13e93c8bda10221631c66a3ee981cf
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-10-31 00:08:48 -0400

    create route to models/tables (raw data)

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from models import db, User, Contractor
+from models import db, User, Contractor, Review, Post, Like, Comment
 
 from flask import Flask, jsonify, request, session
 # from flask_cors import CORS
@@ -15,11 +15,56 @@ app.json.compact = False
 
 migrate = Migrate (app, db)
 
+db.init_app(app)
+
+URL_PREFIX = "/api/v1/admin"
+
 @app.route("/")
 def index ():
     return "<h1>Welcome to the contractors favorite website</h1>"
 
-db.init_app(app)
+@app.get(URL_PREFIX + "/users")
+def get_users ():
+    users = User.query.all()
+    response = [user.to_dict() for user in users]
+    return jsonify(response), 200
+
+@app.get(URL_PREFIX + "/contractors/info")
+def get_contractors ():
+    contractors = Contractor.query.all()
+    response = [contractor.to_dict() for contractor in contractors]
+    return jsonify(response), 200
+    
+@app.get(URL_PREFIX + "/reviews-section")
+def get_reviews ():
+    reviews = Review.query.all()
+    response = [review.to_dict() for review in reviews]
+    return jsonify(response), 200
+
+@app.get(URL_PREFIX + "/posts-section")
+def get_posts ():
+    posts = Post.query.all()
+    response = [post.to_dict() for post in posts]
+    return jsonify(response), 200
+
+@app.get(URL_PREFIX + "/likes-section")
+def get_likes ():
+    likes = Like.query.all()
+    response = [like.to_dict() for like in likes]
+    return jsonify(response), 200
+
+@app.get(URL_PREFIX + "/comments/info")
+def get_comments ():
+    comments = Comment.query.all()
+    response = [comment.to_dict() for comment in comments]
+    return jsonify(response), 200
+
+@app.get(URL_PREFIX + "/saved-post/")
+def  get_posts ():
+    posts = Post.query.all()
+    response = [post.to_dict() for post in Post]
+    return jsonify(response), 200
+
 
 if __name__ == ("__main__"):
     app.run (port=8001, debug=True)
