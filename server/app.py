9635b50b2a2c33917c4b43541ef926c040a2b247
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-11-01 18:47:02 -0400

    create user routes (post/edit/id/delete)

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from models import db, User, Contractor, Review, Post, Like, Comment
+from models import db, User, Contractor, Post
 
 from flask import Flask, jsonify, request, session
 # from flask_cors import CORS
@@ -27,44 +27,96 @@ def index ():
 def get_users ():
     users = User.query.all()
     response = [user.to_dict() for user in users]
+
     return jsonify(response), 200
 
 @app.get(URL_PREFIX + "/contractors/info")
 def get_contractors ():
     contractors = Contractor.query.all()
     response = [contractor.to_dict() for contractor in contractors]
-    return jsonify(response), 200
     
+    return jsonify(response), 200
+
+"""    
 @app.get(URL_PREFIX + "/reviews-section")
 def get_reviews ():
+    "<h4>REVIEWS route</h4>"
     reviews = Review.query.all()
     response = [review.to_dict() for review in reviews]
-    return jsonify(response), 200
+    return jsonify(response), 200"""
 
 @app.get(URL_PREFIX + "/posts-section")
 def get_posts ():
     posts = Post.query.all()
     response = [post.to_dict() for post in posts]
+    
     return jsonify(response), 200
 
-@app.get(URL_PREFIX + "/likes-section")
+"""@app.get(URL_PREFIX + "/likes-section")
 def get_likes ():
+    "<h4>LIKES route</h4>"
     likes = Like.query.all()
     response = [like.to_dict() for like in likes]
-    return jsonify(response), 200
+    return jsonify(response), 200"""
 
-@app.get(URL_PREFIX + "/comments/info")
+"""@app.get(URL_PREFIX + "/comments/info")
 def get_comments ():
+    "<h4>COMMENTS route</h4>"
     comments = Comment.query.all()
     response = [comment.to_dict() for comment in comments]
-    return jsonify(response), 200
+    return jsonify(response), 200"""
 
-@app.get(URL_PREFIX + "/saved-post/")
+
+"""@app.get(URL_PREFIX + "/saved-post/")
 def  get_posts ():
+    "<h4SAVED POSTS route></h4>"
     posts = Post.query.all()
-    response = [post.to_dict() for post in Post]
-    return jsonify(response), 200
+    response = [post.to_dict() for post in posts]
+    return jsonify(response), 200"""
 
+@app.get(URL_PREFIX + "/user/<int:id>")
+def get_user_id (id):
+    try:
+        user = User.query.filter(User.id == id).first()
+        return jsonify(user.to_dict()), 200
+    except:
+        return ({"error": "Episode not found"}), 404
 
-if __name__ == ("__main__"):
+@app.post (URL_PREFIX + "/new-user")
+def create_user ():
+    data = request.json
+    new_user = User(**data)
+    db.session.add(new_user)
+    db.session.commit()
+    print(data)
+    return new_user.to_dict(), 201
+
+@app.delete(URL_PREFIX + '/users/<int:id>')
+def delete_user (id):
+    try:
+        user = User.query.filter(User.id == id).first()
+        db.session.delete(user)
+        db.commit()
+        return {}, 204
+    except:
+        return ({"error": "User not found"}), 404
+    
+@app.patch(URL_PREFIX + "/user-edit/<int:id>")
+def edit_user(id):
+    data = request.json
+    print(data)
+    User.query.filter(User.id == id).update(data)
+    user = User.query.filter(User.id == id).first()
+    
+    db.session.add(user)
+    db.session.commit()
+    
+    return user.to_dict(),200
+
+"""
+creating serialize rules for the route (what to show on the browser when the API load)
+# self.to_dict(rules=("-contractor.address",))
+"""
+
+if __name__ == ('__main__'):
     app.run (port=8001, debug=True)
